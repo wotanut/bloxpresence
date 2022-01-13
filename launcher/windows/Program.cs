58e4d33272da55dcb92992c67238f0f02fc3ce28
Sam <75843086+wotanut@users.noreply.github.com>
2022-01-13 10:53:35 +0000

    Add files via upload

---
+++ launcher/windows/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process launch = new Process();
+
+            launch.StartInfo.UseShellExecute = false;
+            launch.StartInfo.FileName = "../bloxpresence/setup.py";
+            launch.StartInfo.CreateNoWindow = true;
+            launch.Start();
+        }
+    }
+}
